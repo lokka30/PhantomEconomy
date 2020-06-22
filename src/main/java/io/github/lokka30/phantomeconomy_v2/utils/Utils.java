f37906bc8acad31053e3f2661043b8d707795615
lokka30 <lokka30@protonmail.com>
2020-06-22 10:40:52 +0800

    1.9.26-RELEASE ** Changes to v2

--- src/main/java/io/github/lokka30/phantomeconomy_v2/utils/Utils.java
+++ src/main/java/io/github/lokka30/phantomeconomy_v2/utils/Utils.java
@@ -5,6 +5,7 @@ import org.bukkit.ChatColor;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.logging.Logger;
 
 public class Utils {
 
@@ -17,15 +18,16 @@ public class Utils {
 
     public void log(final LogLevel logLevel, String msg) {
         msg = colorize("&b&lPhantomEconomy: &7" + msg);
+        Logger logger = Bukkit.getLogger();
         switch (logLevel) {
             case INFO:
-                Bukkit.getLogger().info(msg);
+                logger.info(msg);
                 break;
             case WARNING:
-                Bukkit.getLogger().warning(msg);
+                logger.warning(msg);
                 break;
             case SEVERE:
-                Bukkit.getLogger().severe(msg);
+                logger.severe(msg);
                 break;
             default:
                 throw new IllegalStateException("Illegal LogLevel state: " + logLevel.toString());
@@ -34,42 +36,16 @@ public class Utils {
 
     public List<String> getSupportedServerVersions() {
         return Arrays.asList(
-                "1.15.2",
-                "1.15.1",
+                "1.16",
                 "1.15",
-                "1.14.4",
-                "1.14.3",
-                "1.14.2",
-                "1.14.1",
-                "1.14",
-                "1.13.2",
-                "1.13.1",
+                "1.14,",
                 "1.13",
-                "1.12.2",
-                "1.12.1",
                 "1.12",
-                "1.11.2",
-                "1.11.1",
                 "1.11",
-                "1.10.2",
-                "1.10.1",
                 "1.10",
-                "1.9.4",
-                "1.9.3",
-                "1.9.2",
-                "1.9.1",
                 "1.9",
-                "1.8.9",
-                "1.8.8",
-                "1.8.7",
-                "1.8.6",
-                "1.8.5",
-                "1.8.4",
-                "1.8.3",
-                "1.8.2",
-                "1.8.1",
                 "1.8",
-                "1.7.10"
+                "1.7"
         );
     }
 
