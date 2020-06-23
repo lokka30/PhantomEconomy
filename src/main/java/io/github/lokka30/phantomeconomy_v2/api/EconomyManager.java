a32fe4c2d027319d47f99ea97ec7257912f99ce9
lokka30 <lokka30@protonmail.com>
2020-06-23 09:27:28 +0800

    1.9.27-RELEASE
    ** Changes to v2

--- src/main/java/io/github/lokka30/phantomeconomy_v2/api/EconomyManager.java
+++ src/main/java/io/github/lokka30/phantomeconomy_v2/api/EconomyManager.java
@@ -29,28 +29,28 @@ public class EconomyManager {
 
     // Check if the currency 'name' is enabled
     public boolean isCurrency(String name) {
-        return instance.fileCache.SETTINGS_CURRENCIES_ENABLED_CURRENCIES.contains(name);
+        return instance.getFileCache().SETTINGS_CURRENCIES_ENABLED_CURRENCIES.contains(name);
     }
 
     // Returns the default currency
     @SuppressWarnings("unused")
     public Currency getDefaultCurrency() throws InvalidCurrencyException {
-        String defaultCurrencyName = instance.fileCache.SETTINGS_DEFAULT_CURRENCY;
+        String defaultCurrencyName = instance.getFileCache().SETTINGS_DEFAULT_CURRENCY;
         if (isCurrency(defaultCurrencyName)) {
             return getCurrency(defaultCurrencyName);
         } else {
-            instance.utils.log(LogLevel.SEVERE, "&cSETTINGS ERROR! &7Unable to get currency '" + defaultCurrencyName + "' as was specified as the default currency. Check the default currency setting and make sure that the currency is enabled and defined. This should be fixed immediately as major issues could occur.");
+            instance.getUtils().log(LogLevel.SEVERE, "&cSETTINGS ERROR! &7Unable to get currency '" + defaultCurrencyName + "' as was specified as the default currency. Check the default currency setting and make sure that the currency is enabled and defined. This should be fixed immediately as major issues could occur.");
             return null;
         }
     }
 
     // Returns the currency that Vault is used for
     public Currency getVaultCurrency() throws InvalidCurrencyException {
-        String vaultCurrencyName = instance.fileCache.SETTINGS_VAULT_CURRENCY;
+        String vaultCurrencyName = instance.getFileCache().SETTINGS_VAULT_CURRENCY;
         if (isCurrency(vaultCurrencyName)) {
             return getCurrency(vaultCurrencyName);
         } else {
-            instance.utils.log(LogLevel.SEVERE, "&cSETTINGS ERROR! &7Unable to get currency '" + vaultCurrencyName + "' as was specified as the Vault currency. Check the vault currency setting and make sure that the currency is enabled and defined. This should be fixed immediately as it is likely to cause major issues with the plugins trying to use the Vault API.");
+            instance.getUtils().log(LogLevel.SEVERE, "&cSETTINGS ERROR! &7Unable to get currency '" + vaultCurrencyName + "' as was specified as the Vault currency. Check the vault currency setting and make sure that the currency is enabled and defined. This should be fixed immediately as it is likely to cause major issues with the plugins trying to use the Vault API.");
             return null;
         }
     }
